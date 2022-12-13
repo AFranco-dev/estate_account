d5eb679f979898092e893adc274a70d2b81c2939
Andre Franco <andre.franco.dev@gmail.com>
2022-12-13 16:58:19 -0600

    added skeleton

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Real Estate Invoicing',
+    'version': '1.0.0',
+    'category': 'Real Estate',
+    'sequence': -999,
+    'summary': 'A module for managing your real estate properties',
+    'description': "This is the odoo 16 tutorial module",
+    'depends': [
+        'base',
+        'mail',
+        'account',
+        'real-estate',
+    ],
+    'data': [
+    ],
+    'demo': [],
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
