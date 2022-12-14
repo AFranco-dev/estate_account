0728b4f200ddfd13e78ecf9b8e3490982b60491c
Andre Franco <andre.franco.dev@gmail.com>
2022-12-14 15:49:30 -0600

    fixed invoice creation

--- models/estate_property.py
+++ models/estate_property.py
@@ -5,21 +5,20 @@ class EstateProperty(models.Model):
     _inherit = 'estate.property'
 
     def property_sold(self):
-        for rec in self:
-            # defining invoice fields
-            partner_id = rec.buyer_id.id
-            move_type = 'out_invoice'
-            print(rec.env['account.move'].name)
-            journal_id = self.env['account.move'].with_context(move_type='out_invoice')._search_default_journal().id
+        # defining invoice fields
+        partner_id = self.buyer_id.id
+        move_type = 'out_invoice'
+        print(self.env['account.move'].name)
+        journal_id = self.env['account.move'].with_context(move_type='out_invoice')._search_default_journal().id
 
-            # creating invoice dictionary
-            invoice = {
-                'partner_id': partner_id,
-                'move_type': move_type,
-                'journal_id': journal_id,
-            }
+        # creating invoice dictionary
+        invoice = {
+            'partner_id': partner_id,
+            'move_type': move_type,
+            'journal_id': journal_id,
+        }
 
-            self.env['account.move'].sudo().with_context(default_move_type='out_invoice').create(invoice)
+        self.env['account.move'].sudo().with_context(default_move_type='out_invoice').create(invoice)
 
 
         return super().property_sold()
