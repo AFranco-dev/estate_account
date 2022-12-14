0a5d9e71ae62a0dced3c982cab057d6f822d90da
Andre Franco <andre.franco.dev@gmail.com>
2022-12-14 09:57:28 -0600

    fixing overriden inherited method

--- models/estate_property.py
+++ models/estate_property.py
@@ -4,7 +4,7 @@ from odoo.tools import date_utils
 from odoo.tools import float_utils
 
 class EstateProperty(models.Model):
-    _inherit = ['mail.thread', 'mail.activity.mixin', 'estate.property']
+    _inherit = 'estate.property'
 
     def property_sold(self):
         print("Overriden property sold. AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
