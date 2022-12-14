45177826c2643368d7e58dd94131e044b4dad51c
Andre Franco <andre.franco.dev@gmail.com>
2022-12-14 09:08:01 -0600

    overriden property sold method

---
+++ models/estate_property.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, exceptions
+from odoo.tools import date_utils
+from odoo.tools import float_utils
+
+class EstateProperty(models.Model):
+    _inherit = ['mail.thread', 'mail.activity.mixin', 'estate.property']
+
+    def property_sold(self):
+        print("Overriden property sold.")
+        return super().property_sold()
