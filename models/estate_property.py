c0c5812608903f9f0a700e2a2698e92d406a8950
Andre Franco <andre.franco.dev@gmail.com>
2022-12-14 14:23:03 -0600

    fixed invoice creation

--- models/estate_property.py
+++ models/estate_property.py
@@ -4,26 +4,21 @@ from odoo import api, fields, models, exceptions
 class EstateProperty(models.Model):
     _inherit = 'estate.property'
 
-    def _prepare_invoice_dict(self):
-        journal = self.env['account.move'].with_context(default_move_type='out_invoice')._get_default_journal()
-        self.ensure_one()
-        invoice_vals = {
-            'partner_id': self.buyer_id.id,
-            'move_type': 'out_invoice',
-            'journal_id': journal.id
-        }
-        return invoice_vals
-    
-    def _create_invoices(self, grouped=False, final=True, date=None):
-        invoice = self._prepare_invoice_dict
-        
-        if not invoice:
-            raise exceptions.UserError("No invoices to issue.")
-
-        moves = self.env['account.move'].sudo().with_context(default_move_type='out_invoice').create(invoice)
-
-        return moves
-
     def property_sold(self):
-        self._create_invoices()
+        for rec in self:
+            # defining invoice fields
+            partner_id = rec.buyer_id.id
+            move_type = 'out_invoice'
+            journal_id = self.env['account.move'].with_context(default_move_type='out_invoice')._get_default_journal().id
+
+            # creating invoice dictionary
+            invoice = {
+                'partner_id': partner_id,
+                'move_type': move_type,
+                'journal_id': journal_id,
+            }
+
+            self.env['account.move'].sudo().with_context(default_move_type='out_invoice').create(invoice)
+
+
         return super().property_sold()
