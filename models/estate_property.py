a98cec00e43d08debb020f9a0a95edd29bcc21b3
Andre Franco <andre.franco.dev@gmail.com>
2022-12-14 11:58:23 -0600

    added invoicing skeleton

--- models/estate_property.py
+++ models/estate_property.py
@@ -1,11 +1,33 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, exceptions
-from odoo.tools import date_utils
-from odoo.tools import float_utils
 
 class EstateProperty(models.Model):
     _inherit = 'estate.property'
 
+    def _prepare_invoice_dict(self):
+        journal = self.env['account.move'].with_context(default_move_type='out_invoice')._get_default_journal()
+        self.ensure_one()
+        invoice_vals = {
+            'partner_id': self.buyer_id.id,
+            'move_type': 'out_invoice',
+            'journal_id': journal.id
+        }
+        return invoice_vals
+    
+    def _create_invoices(self, grouped=False, final=True, date=None):
+        invoice_vals_list = []
+
+        for order in self:
+            invoice_vals = self._prepare_invoice_dict
+            invoice_vals_list.append(invoice_vals)
+        
+        if not invoice_vals_list:
+            raise self._nothing_to_invoice_error()
+
+        moves = self.env['account.move'].sudo().with_context(default_move_type='out_invoice').create(invoice_vals_list)
+
+        return moves
+
     def property_sold(self):
-        print("Overriden property sold. AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+
         return super().property_sold()
