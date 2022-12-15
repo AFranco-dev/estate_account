2670d7ea7382768acec631458b5a3f0ef5b04915
Andre Franco <andre.franco.dev@gmail.com>
2022-12-14 20:59:03 -0600

    fixed invoice creation

--- models/estate_property.py
+++ models/estate_property.py
@@ -9,16 +9,11 @@ class EstateProperty(models.Model):
         # defining invoice fields
         partner_id = self.buyer_id.id
         move_type = 'out_invoice'
-        journal = self.env['account.move'].with_context(default_move_type='out_invoice')._get_default_journal()
-        print(journal)
-        print(self.env['account.move'].search([('model_type', '=', 'out_invoice')]))
-        journal_id = self.env['account.move'].sudo().with_context(move_type='out_invoice').id
 
         # creating invoice dictionary
         invoice = {
             'partner_id': partner_id,
             'move_type': move_type,
-            'journal_id': journal_id,
         }
 
         self.env['account.move'].sudo().with_context(default_move_type='out_invoice').create(invoice)
